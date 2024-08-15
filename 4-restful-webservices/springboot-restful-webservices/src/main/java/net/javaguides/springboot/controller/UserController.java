64aa78dbbd3dd4b06fd84959be02c082bec2e28d
Himanshu Abburu <himanshuabburu@gmail.com>
2024-08-15 21:48:57 +0100

    Update Restful API with JPA connections

---
+++ 4-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,54 @@
+package net.javaguides.springboot.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+    private UserService userService;
+
+    // Build create User REST API
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    // Get user REST API
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // Get all users REST API
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long userID,
+                                           @RequestBody User user)
+    {
+        user .setId(userID);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id")  Long id){
+        userService.deleteUSer(id);
+        return new ResponseEntity<>("User Successfully deleted!", HttpStatus.OK);
+    }
+}
