6732798cebc23a33672980b09ae782513fa8779d
Himanshu Abburu <himanshuabburu@gmail.com>
2024-11-10 17:37:12 +0000

    Update to add validation features

--- 4-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
+++ 4-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
 import net.javaguides.springboot.entity.User;
@@ -23,7 +24,7 @@ public class UserController {
 
     // Build create User REST API
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
@@ -44,7 +45,7 @@ public class UserController {
 
     @PutMapping("{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userID,
-                                           @RequestBody UserDto user)
+                                              @Valid @RequestBody UserDto user)
     {
         user .setId(userID);
         UserDto updatedUser = userService.updateUser(user);
--- 4-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
+++ 4-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
+
     private Long id;
+
+    @NotEmpty(message = "User First name cannot be empty or null")
     private String firstName;
+
+    @NotEmpty(message = "User Last name cannot be empty or null")
     private String lastName;
+
+    @NotEmpty(message = "User email cannot be empty or null and must be well formed")
+    @Email(message = "User email must be well formed")
     private String email;
 }
